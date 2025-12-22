b53c96fa6af406a1a7d36d8dbaa60cb5e4c3711f
kajaldhobale <Kajaldhobale21@gmail.com>
2025-12-22 08:56:59 +0530

    first commit

--- Project/V1.EC2.tf
+++ Project/V1.EC2.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = "ap-south-1"
 }   
 
-resource "aws_instance" "demo-sg" {
+resource "aws_instance" "demo-server" {
     ami = "ami-0c44f651ab5e9285f"
     instance_type = "t3.micro"
-    key_name = "Demo1"
-    security_groups = ["SSH-Access"]
+    key_name = "Demo3"
+    vpc_security_group_ids = [aws_security_group.demo_sg1.id]
 }
 
-resource "aws_security_group" "SSH-Access" {
-    name = "ssh-access"
+resource "aws_security_group" "demo_sg1" {
+    name = "demo-sg1"
     description = "Security group to allow SSH access"
 
     ingress {
